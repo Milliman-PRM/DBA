53cc16a4495e1e5739a1e4cde522e7474c055ca1
Ben Wyatt <Ben.Wyatt@milliman.com>
2016-08-05 14:47:22 -0400

    Switched from using os library to using pathlib
    Only saves a couple lines, but it feels cleaner & more readable this
    way.

--- Scripts/mongodb/PRM-MongoDB01/folder_cleanup.py
+++ Scripts/mongodb/PRM-MongoDB01/folder_cleanup.py
@@ -7,17 +7,17 @@
 ### DEVELOPER NOTES:
 
 """
-import os
 import re
 import time
 import shutil
+from pathlib import Path
 
 # number of seconds in a day * number of days of folders to keep
 NUMDAYS = 86400*15
 NOW = time.time()
 
 # Path to the folder to be cleaned up
-CHECK_DIRECTORY = os.path.abspath(r"C:\PRM-CDR_Archive")
+CHECK_DIRECTORY = r"C:\PRM-CDR_Archive"
 TARGET_DIRECTORY = r"\\indy-backup\PRM-MONGODB\PRM-MongoDB01"
 
 print("Copying folders from ", CHECK_DIRECTORY, " to ", TARGET_DIRECTORY)
@@ -28,17 +28,14 @@ FOLDER_PATTERN = "^20[0-9]{2}y-[0-9]{2}m-[0-9]{2}d@[0-9]+h-[0-9]{2}mPT$"
 
 # Recurse over the indicated directory
 # Remove folders that match the regex and age
-for r, d, f in os.walk(CHECK_DIRECTORY):
-    for foldername in d:
-        full_path = os.path.join(r, foldername)
-        timestamp = os.path.getmtime(full_path)
-        if NOW-NUMDAYS > timestamp and re.match(FOLDER_PATTERN, foldername):
-            try:
-                print("Moving ", full_path)
-                shutil.move(full_path, TARGET_DIRECTORY)
-            except shutil.Error as err:
-                print(err)
-            else:
-                print("success\n\n")
+for folder in Path(CHECK_DIRECTORY).glob('20*/'):
+    if NOW-NUMDAYS > folder.stat().st_mtime and re.match(FOLDER_PATTERN, folder.stem):
+        try:
+            print("Moving", folder)
+            shutil.move(folder, TARGET_DIRECTORY)
+        except shutil.Error as err:
+            print(err)
         else:
-            print("not removing", full_path, "\n")
+            print("success\n\n")
+    else:
+        print("not moving", folder, "\n")
