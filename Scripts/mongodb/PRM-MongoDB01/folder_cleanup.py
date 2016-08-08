ffad448574fe26007e26237252b48c770c9220a7
Ben Wyatt <Ben.Wyatt@milliman.com>
2016-08-08 15:13:07 -0400

    Clean up print formatting and switch to updated collect_files_regex
    The updated collect_files_regex is a pending change that we are
    reviewing for IndyMacros.

--- Scripts/mongodb/PRM-MongoDB01/folder_cleanup.py
+++ Scripts/mongodb/PRM-MongoDB01/folder_cleanup.py
@@ -10,6 +10,7 @@
 import re
 import time
 import shutil
+#from pathlib import Path
 from indypy.file_utils import IndyPyPath
 
 # number of seconds in a day * number of days of folders to keep
@@ -20,15 +21,15 @@ NOW = time.time()
 CHECK_DIRECTORY = r"C:\PRM-CDR_Archive"
 TARGET_DIRECTORY = r"\\indy-backup\PRM-MONGODB\PRM-MongoDB01"
 
-print("Copying folders from ", CHECK_DIRECTORY, " to ", TARGET_DIRECTORY)
+print("Copying folders from", CHECK_DIRECTORY, "to", TARGET_DIRECTORY)
 print("")
 
 # Regular expression to match folder names to be removed
-FOLDER_PATTERN = "^20[0-9]{2}y-[0-9]{2}m-[0-9]{2}d@[0-9]+h-[0-9]{2}mPT$"
+FOLDER_PATTERN = "20[0-9]{2}y-[0-9]{2}m-[0-9]{2}d@[0-9]+h-[0-9]{2}mPT"
 
 # Recurse over the indicated directory
 # Remove folders that match the regex and age
-for folder in IndyPyPath(CHECK_DIRECTORY).collect_files_regex(FOLDER_PATTERN):
+for folder in IndyPyPath(CHECK_DIRECTORY).collect_files_regex(FOLDER_PATTERN, collect_dirs = True):
     if NOW-NUMDAYS > folder.stat().st_mtime:
         try:
             print("Moving", folder)
@@ -36,6 +37,6 @@ for folder in IndyPyPath(CHECK_DIRECTORY).collect_files_regex(FOLDER_PATTERN):
         except shutil.Error as err:
             print(err)
         else:
-            print("success\n\n")
+            print("success\n")
     else:
         print("not moving", folder, "\n")
