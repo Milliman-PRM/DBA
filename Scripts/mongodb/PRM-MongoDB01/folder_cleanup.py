6b7e4b0286757d5f02a399f1aaf140300b553b1e
Ben Wyatt <Ben.Wyatt@milliman.com>
2016-08-09 09:33:14 -0400

    Switch back to previous version of folder_cleanup.py
    Some minor formatting changes were made here, but generally this is the
    same as it was before we tried integrating (and changing) IndyPy.

--- Scripts/mongodb/PRM-MongoDB01/folder_cleanup.py
+++ Scripts/mongodb/PRM-MongoDB01/folder_cleanup.py
@@ -1,17 +1,13 @@
 """
 ## CODE OWNERS: Ben Wyatt, Steve Gredell
-
 ### OBJECTIVE:
   Clean up outdated backup files from PRM-MongoDB01
-
 ### DEVELOPER NOTES:
-
 """
 import re
 import time
 import shutil
-#from pathlib import Path
-from indypy.file_utils import IndyPyPath
+from pathlib import Path
 
 # number of seconds in a day * number of days of folders to keep
 NUMDAYS = 86400*15
@@ -25,12 +21,12 @@ print("Copying folders from", CHECK_DIRECTORY, "to", TARGET_DIRECTORY)
 print("")
 
 # Regular expression to match folder names to be removed
-FOLDER_PATTERN = "20[0-9]{2}y-[0-9]{2}m-[0-9]{2}d@[0-9]+h-[0-9]{2}mPT"
+FOLDER_PATTERN = "^20[0-9]{2}y-[0-9]{2}m-[0-9]{2}d@[0-9]+h-[0-9]{2}mPT$"
 
 # Recurse over the indicated directory
 # Remove folders that match the regex and age
-for folder in IndyPyPath(CHECK_DIRECTORY).collect_files_regex(FOLDER_PATTERN, collect_dirs = True):
-    if NOW-NUMDAYS > folder.stat().st_mtime:
+for folder in Path(CHECK_DIRECTORY).glob('20*/'):
+    if NOW-NUMDAYS > folder.stat().st_mtime and re.match(FOLDER_PATTERN, folder.stem):
         try:
             print("Moving", folder)
             shutil.move(folder, TARGET_DIRECTORY)
