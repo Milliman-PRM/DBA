a9ae4295c99f49a86d1797398f0034b6e5d54b77
Ben Wyatt <Ben.Wyatt@milliman.com>
2016-08-19 14:50:08 -0400

    Add exception handling, end up in indentation hell
    That'll teach me not to do it from the beginning...

--- Scripts/pgsql/dump_schema.py
+++ Scripts/pgsql/dump_schema.py
@@ -32,46 +32,49 @@ def execute_dump(
     path_to_git = "L:/Hotware/git/PortableGit_2.5.3.windows.1.github.0/cmd/git.exe"
     path_to_pgdumpall = "L:/Hotware/Postgresql/pg_dumpall.exe"
 
-    # Clone the DBA repo here
-    #subprocess.call([path_to_git, "clone", repo_url])
+    try:
 
-    # Run pg_dumpall to dump
-    subprocess.check_call([path_to_pgdumpall, "--host", target_server, "--port", "5432",
-                           "--username", pgsql_username, "--password", pgsql_password,
-                           "--verbose", "--file", target_file, "--no-tablespaces",
-                           "--schema-only"])
+        # Run pg_dumpall to dump
+        subprocess.check_call([path_to_pgdumpall, "--host", target_server, "--port", "5432",
+                               "--username", pgsql_username, "--password", pgsql_password,
+                               "--verbose", "--file", target_file, "--no-tablespaces",
+                               "--schema-only"])
 
-    """
-    Remove lines from the file if they match this expression
+        """
+        Remove lines from the file if they match this expression
 
-    This allows us to make sure timestamps aren't flagged as
-        changes by git
+        This allows us to make sure timestamps aren't flagged as
+            changes by git
 
-    Expression is as specific as possible to ensure only exact matches get
-        stripped out of the file
-    """ # pylint: disable=pointless-string-statement
-    timestamp_expression = r"^-- ((Started)|(Completed)) on 20[0-9]{2}-", \
-    "[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$"
+        Expression is as specific as possible to ensure only exact matches get
+            stripped out of the file
+        """ # pylint: disable=pointless-string-statement
+        timestamp_expression = r"^-- ((Started)|(Completed)) on 20[0-9]{2}-", \
+        "[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$"
 
-    for line in fileinput.input(target_file, inplace=True):
-        if not re.match(timestamp_expression, line):
-            print line,
+        for line in fileinput.input(target_file, inplace=True):
+            if not re.match(timestamp_expression, line):
+                print line,
 
-    # Change to the repo directory to issue git commands
-    os.chdir(os.path.relpath("DBA"))
+        # Change to the repo directory to issue git commands
+        os.chdir(os.path.relpath("DBA"))
 
-    # Check for changes
-    process_output = subprocess.check_output([path_to_git, "status", "--porcelain"])
-    print process_output
+        # Check for changes
+        process_output = subprocess.check_output([path_to_git, "status", "--porcelain"])
+        print process_output
 
-    # Commit and push back to master if any changes are found
-    if len(process_output.strip()) > 0:
-        commit_message = "Updated schema documentation for " + target_server
-        subprocess.check_call([path_to_git, "add", "*"])
-        subprocess.check_call([path_to_git, "commit", "-am", str(commit_message)])
-        subprocess.check_call([path_to_git, "push", "--repo=" + repo_url])
-    else:
-        print "No changes found"
+        # Commit and push back to master if any changes are found
+        if len(process_output.strip()) > 0:
+            commit_message = "Updated schema documentation for " + target_server
+            subprocess.check_call([path_to_git, "add", "*"])
+            subprocess.check_call([path_to_git, "commit", "-am", str(commit_message)])
+            subprocess.check_call([path_to_git, "push", "--repo=" + repo_url])
+        else:
+            print "No changes found"
+
+    except subprocess.CalledProcessError as err:
+        print err
+        raise
 
     # Clean up folder, if necessary
 
