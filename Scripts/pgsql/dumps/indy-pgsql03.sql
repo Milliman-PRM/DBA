3f42870f471fdc1f8197f61c9265dc45d69450ce
indy-jenkins <indy-jenkins@milliman.com>
2017-04-22 02:05:07 -0400

    Updated schema for indy-pgsql03

--- Scripts/pgsql/dumps/indy-pgsql03.sql
+++ Scripts/pgsql/dumps/indy-pgsql03.sql
@@ -250,7 +250,6 @@ GRANT "Indy_ePHI_0032CCS" TO "michael.reisz" GRANTED BY postgres;
 GRANT "Indy_ePHI_0032CCS" TO "shea.parkes" GRANTED BY postgres;
 GRANT "Indy_ePHI_0032FRH" TO "michael.reisz" GRANTED BY postgres;
 GRANT "Indy_ePHI_0032ILM" TO "brad.teach" GRANTED BY postgres;
-GRANT "Indy_ePHI_0032ILM" TO "shea.parkes" GRANTED BY postgres;
 GRANT "Indy_ePHI_0032IWM" TO "shea.parkes" GRANTED BY postgres;
 GRANT "Indy_ePHI_0032KYM" TO "kelsie.stevenson" GRANTED BY postgres;
 GRANT "Indy_ePHI_0032MFN" TO "jason.altieri" GRANTED BY postgres;
