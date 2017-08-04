c045c52aa86d95c7a5124f64b8684f9ed4321d66
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-08-04 14:08:43 -0400

    Add script to pull export counts for NY reports

---
+++ Scripts/pgsql/Indy-PGSQL01/systemreporting_ny_usage.sql
@@ -0,0 +1,16 @@
+select 	usr.username,
+		split_part(document, '\', 3) as ReportName,
+        substring(message from '(\w*\.csv)') as FileName, 
+		count(qvauditlog.id)
+
+from qvauditlog
+		inner join "user" as usr on qvauditlog.fk_user_id = usr.id
+
+where document like '0000EXT01\\NEWYORK\\%'
+		and useraccessdatetime >= date_trunc('month', current_date - interval '1' month)
+  		and useraccessdatetime < date_trunc('month', current_date)
+        and message like 'action(11)%'
+
+group by usr.username, message, document
+               
+order by username, reportname, filename;
