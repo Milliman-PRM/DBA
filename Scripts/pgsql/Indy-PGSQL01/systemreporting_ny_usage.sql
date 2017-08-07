898cba460e7a790d6fca26215f3db191ee11b713
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-08-07 11:30:09 -0400

    Add date ranges to NY export script

--- Scripts/pgsql/Indy-PGSQL01/systemreporting_ny_usage.sql
+++ Scripts/pgsql/Indy-PGSQL01/systemreporting_ny_usage.sql
@@ -1,7 +1,9 @@
 select 	usr.username,
 		split_part(document, '\', 3) as ReportName,
-        substring(message from '(\w*\.csv)') as FileName, 
-		count(qvauditlog.id)
+        substring(message from '(\w*\.csv)') as FileName,
+		count(qvauditlog.id),
+		useraccessdatetime >= date_trunc('month', current_date - interval '1' month) as StartDate,
+		and useraccessdatetime < date_trunc('month', current_date) as EndDate
 
 from qvauditlog
 		inner join "user" as usr on qvauditlog.fk_user_id = usr.id
@@ -12,5 +14,5 @@ where document like '0000EXT01\\NEWYORK\\%'
         and message like 'action(11)%'
 
 group by usr.username, message, document
-               
+
 order by username, reportname, filename;
